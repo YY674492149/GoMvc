960df15fc0c8bd018e6f51e1d9b8da88cffc020c
YY <674492149@qq.com>
2017-12-12 11:15:31 +0800

    修改第二分支

--- ceshiyong/ceshiyong/Class1.cs
+++ ceshiyong/ceshiyong/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ceshiyong
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("第二分之");
-        }
     }
 }
